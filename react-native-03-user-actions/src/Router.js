cffd9e759bf3360f9c420b8b77c3d0dfe3ac4ad6
Damla Erva Kasal <damlaervakasal3@gmail.com>
2023-12-17 17:58:53 +0300

    react-navigation and native stack navigator library installed

---
+++ react-native-03-user-actions/src/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator></Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
