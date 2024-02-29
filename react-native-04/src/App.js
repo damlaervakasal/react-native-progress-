a86106194fbb9ce0f2f44ce5f7875cf07392b570
Damla Erva Kasal <damlaervakasal3@gmail.com>
2024-03-01 00:56:44 +0300

    Understanding promise analysis using the 'async&wait' structure.

--- react-native-04/src/App.js
+++ react-native-04/src/App.js
@@ -3,12 +3,31 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import axios from "axios";
 
 function App() {
-  function fetchData() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
+  // promise cozumleme ---- then&catch
+
+  // function fetchData() {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => console.log(response))
+  //     .catch((error) => console.log(error));
+  // }
+  // cevap gelinceye kadar beklemiyor. cagrıyı atip direkt alt satira iniyor.
+
+  // promise cozumleme ---- asyn&await
+  async function fetchData() {
+    console.log("1");
+
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log("3");
+    console.log(response);
+
+    console.log("2");
   }
+  // burada 1 bekle 3 2 cıktısını verir yani bana cevabin dönmesi için
+  // cevap gelinceye kadar bekletiyorum. cevap geldikten sonra alt satira geciyor.
+
   return (
     <View style={styles.container}>
       <Text>Hello API</Text>
@@ -28,3 +47,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+// birinin diğerine gore avantajı yoktur.
+// kod blogunu bekletmek istemiyorsak thencatch,
+// beklemek zorundaysak async&await kullanmak daha dogrudur.
